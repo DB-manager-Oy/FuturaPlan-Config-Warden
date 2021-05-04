6d35f37ec4ff665922cbc199140fc659ad703dfb
Henri Hakala <henri.hakala@gmail.com>
2021-05-04 21:30:27 +0300

    App config template now gets reset before every build so modifications dont get lost.

--- FuturaPlanConfigWarden/Windows/FuturaPlanConfigWardenControl.xaml.cs
+++ FuturaPlanConfigWarden/Windows/FuturaPlanConfigWardenControl.xaml.cs
@@ -13,22 +13,17 @@ using Microsoft.Win32;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace FuturaPlanConfigWarden.Windows
-{
-    public partial class FuturaPlanConfigWardenControl : UserControl, INotifyPropertyChanged
-    {
+namespace FuturaPlanConfigWarden.Windows {
+    public partial class FuturaPlanConfigWardenControl : UserControl, INotifyPropertyChanged {
 
         public ObservableCollection<string> DatabaseList { get; set; }
 
         private string m_selectedDatabase;
-        public string SelectedDatabase
-        {
-            get
-            {
+        public string SelectedDatabase {
+            get {
                 return m_selectedDatabase;
             }
-            set
-            {
+            set {
                 m_selectedDatabase = value;
                 m_dbCache.Update(DatabaseList, value); // fite me irl
                 OnPropertyChanged();
@@ -41,15 +36,12 @@ namespace FuturaPlanConfigWarden.Windows
         private WindowState _state;
 
         private ProjectItem m_appConfig;
-        private ProjectItem AppConfig
-        {
-            get
-            {
+        private ProjectItem AppConfig {
+            get {
                 return m_appConfig;
             }
 
-            set
-            {
+            set {
                 m_appConfig = value;
             }
         }
@@ -57,8 +49,7 @@ namespace FuturaPlanConfigWarden.Windows
 
         private XDocument AppConfigTemplate { get; set; }
 
-        private XAttribute ConnectionStringElement
-        {
+        private XAttribute ConnectionStringElement {
             get {
                 return AppConfigTemplate.Root
                         .Elements("connectionStrings")
@@ -77,19 +68,15 @@ namespace FuturaPlanConfigWarden.Windows
         private BuildEvents m_buildEvents;
         private SolutionEvents m_solutionEvents;
 
-        public FuturaPlanConfigWardenControl(WindowState state)
-        {
+        public FuturaPlanConfigWardenControl(WindowState state) {
             Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
             _state = state;
-            m_dbCache = new DbCache();
 
-            if (m_dbCache.TryGetCacheData(out ObservableCollection<string> data, out string active))
-            {
+            m_dbCache = new DbCache();
+            if (m_dbCache.TryGetCacheData(out ObservableCollection<string> data, out string active)) {
                 DatabaseList = data;
                 SelectedDatabase = active;
-            }
-            else
-            {
+            } else {
                 DatabaseList = new ObservableCollection<string>();
             }
 
@@ -101,25 +88,21 @@ namespace FuturaPlanConfigWarden.Windows
             m_buildEvents.OnBuildBegin += BuildStart;
             m_buildEvents.OnBuildDone += BuildEnd;
 
-            m_solutionEvents.Opened += () =>
-            {
+            m_solutionEvents.Opened += () => {
 
-                if (AppConfig != null)
-                {
+                if (AppConfig != null) {
                     return;
                 }
 
-                if (TryGetAppConfig(out ProjectItem aconf))
-                {
-                    InitializeAppConfig(aconf);
+                if (TryGetAppConfig(out ProjectItem aconf)) {
+                    SetAppConfig(aconf);
                     m_originalConnectionString = ConnectionStringElement.Value;
                 }
 
             };
 
-            if (TryGetAppConfig(out ProjectItem aconf))
-            {
-                InitializeAppConfig(aconf);
+            if (TryGetAppConfig(out ProjectItem aconf)) {
+                SetAppConfig(aconf);
                 m_originalConnectionString = ConnectionStringElement.Value;
             }
 
@@ -128,15 +111,12 @@ namespace FuturaPlanConfigWarden.Windows
             InitializeComponent();
         }
 
-        private void InitializeAppConfig(ProjectItem aconf)
-        {
-
+        private void SetAppConfig(ProjectItem aconf) {
             Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
             AppConfig = aconf;
-            foreach (Property prop in AppConfig.Properties)
-            {
-                if (prop.Name == "LocalPath")
-                {
+            foreach (Property prop in AppConfig.Properties) {
+                if (prop.Name == "LocalPath") {
                     AppConfigPath = prop.Value as string;
                 }
             }
@@ -144,23 +124,22 @@ namespace FuturaPlanConfigWarden.Windows
             AppConfigTemplate = XDocument.Load(AppConfigPath);
         }
 
-        private void BuildStart(vsBuildScope Scope, vsBuildAction Action)
-        {
+        private void BuildStart(vsBuildScope Scope, vsBuildAction Action) {
 
-            if (!OverrideConnectionString)
-            {
+            if (!OverrideConnectionString) {
                 return;
             }
-            
-            switch (Action)
-            {
+
+            switch (Action) {
                 case vsBuildAction.vsBuildActionBuild:
                 case vsBuildAction.vsBuildActionRebuildAll:
 
-                    if (string.IsNullOrEmpty(SelectedDatabase))
-                    {
+                    if (string.IsNullOrEmpty(SelectedDatabase)) {
                         return;
                     }
+
+                    // Reload the template before every build so that modifications to the file dont get lost.
+                    AppConfigTemplate = XDocument.Load(AppConfigPath);
 
                     SetConnectionString(string.Format(m_overrideConnectionString, SelectedDatabase));
                     break;
@@ -169,16 +148,12 @@ namespace FuturaPlanConfigWarden.Windows
             }
         }
 
-        private void BuildEnd(vsBuildScope Scope, vsBuildAction Action)
-        {
-
-            if (!OverrideConnectionString)
-            {
+        private void BuildEnd(vsBuildScope Scope, vsBuildAction Action) {
+            if (!OverrideConnectionString) {
                 return;
             }
 
-            switch (Action)
-            {
+            switch (Action) {
                 case vsBuildAction.vsBuildActionBuild:
                 case vsBuildAction.vsBuildActionRebuildAll:
                     SetConnectionString(m_originalConnectionString);
@@ -188,10 +163,8 @@ namespace FuturaPlanConfigWarden.Windows
             }
         }
 
-        private void SetConnectionString(string connstring)
-        {
-            if (AppConfigTemplate == null)
-            {
+        private void SetConnectionString(string connstring) {
+            if (AppConfigTemplate == null) {
                 return;
             }
 
@@ -201,23 +174,19 @@ namespace FuturaPlanConfigWarden.Windows
         }
 
         private static string[] systemdbs = { "master", "msdb", "model", "tempdb" };
-        public List<string> GetDatabaseList()
-        {
+        public List<string> GetDatabaseList() {
             List<string> list = new List<string>();
-            using (SqlConnection con = new("Server=localhost;Database=master;Trusted_Connection=True;"))
-            {
+            using (SqlConnection con = new("Server=localhost;Database=master;Trusted_Connection=True;")) {
                 con.Open();
 
                 using SqlCommand cmd = new SqlCommand("SELECT name from sys.databases", con);
                 using IDataReader dr = cmd.ExecuteReader();
 
-                while (dr.Read())
-                {
+                while (dr.Read()) {
                     string db = dr[0].ToString();
 
                     // Dont add the system databases.
-                    if (systemdbs.Any(x => x == db))
-                    {
+                    if (systemdbs.Any(x => x == db)) {
                         continue;
                     }
 
@@ -228,29 +197,21 @@ namespace FuturaPlanConfigWarden.Windows
             return list;
         }
 
-        public bool TryGetAppConfig(out ProjectItem appConfig)
-        {
+        public bool TryGetAppConfig(out ProjectItem appConfig) {
             Array loadedProjects;
             appConfig = null;
 
-            try
-            {
+            try {
                 Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
                 loadedProjects = (Array)_state.DTE.ActiveSolutionProjects;
-            }
-            catch (Exception)
-            {
+            } catch (Exception) {
                 return false;
             }
 
-            foreach (Project project in loadedProjects)
-            {
-                for (int i = 0; i < project.ProjectItems.Count; i++)
-                {
-                    foreach (ProjectItem item in project.ProjectItems)
-                    {
-                        if (item.Name == "App.config")
-                        {
+            foreach (Project project in loadedProjects) {
+                for (int i = 0; i < project.ProjectItems.Count; i++) {
+                    foreach (ProjectItem item in project.ProjectItems) {
+                        if (item.Name == "App.config") {
                             appConfig = item;
                             return true;
                         }
@@ -261,41 +222,35 @@ namespace FuturaPlanConfigWarden.Windows
             return false;
         }
 
-        private void RestoreDatabase(string database)
-        {
+        private void RestoreDatabase(string database) {
 
             OpenFileDialog fileDialog = new();
 
-            if (fileDialog.ShowDialog() == false)
-            {
+            if (fileDialog.ShowDialog() == false) {
                 return;
             }
 
-            using (SqlConnection cn = new("Server=localhost;Database=master;Trusted_Connection=True;"))
-            {
+            using (SqlConnection cn = new("Server=localhost;Database=master;Trusted_Connection=True;")) {
                 cn.Open();
                 cn.StatisticsEnabled = true;
 
                 #region step 1 SET SINGLE_USER WITH ROLLBACK
                 string sql = "IF DB_ID('" + database + "') IS NOT NULL ALTER DATABASE [" + database + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE";
-                using (var command = new SqlCommand(sql, cn))
-                {
+                using (var command = new SqlCommand(sql, cn)) {
                     command.ExecuteNonQuery();
                 }
                 #endregion
 
                 #region step 2 Restore
                 sql = "RESTORE DATABASE [" + database + "] FROM DISK='" + fileDialog.FileName + "' WITH FILE = 1, NOUNLOAD";
-                using (var command = new SqlCommand(sql, cn))
-                {
+                using (var command = new SqlCommand(sql, cn)) {
                     command.ExecuteNonQuery()
                 }
                 #endregion
 
                 #region step 3 SET MULTI_USER
                 sql = "ALTER DATABASE [" + database + "] SET MULTI_USER";
-                using (var command = new SqlCommand(sql, cn))
-                {
+                using (var command = new SqlCommand(sql, cn)) {
                     command.ExecuteNonQuery();
                 }
                 #endregion
@@ -305,22 +260,17 @@ namespace FuturaPlanConfigWarden.Windows
         }
 
         private RelayCommand<object> m_refreshDatabaseListCommand;
-        public RelayCommand<object> RefreshDatabaseListCommand
-        {
-            get
-            {
+        public RelayCommand<object> RefreshDatabaseListCommand {
+            get {
 
-                return m_refreshDatabaseListCommand ??= new RelayCommand<object>(_ =>
-                {
+                return m_refreshDatabaseListCommand ??= new RelayCommand<object>(_ => {
                     DatabaseList.Clear();
 
-                    foreach (string dbname in GetDatabaseList().OrderBy(x => x))
-                    {
+                    foreach (string dbname in GetDatabaseList().OrderBy(x => x)) {
                         DatabaseList.Add(dbname);
                     }
 
-                    if (string.IsNullOrEmpty(SelectedDatabase))
-                    {
+                    if (string.IsNullOrEmpty(SelectedDatabase)) {
                         SelectedDatabase = DatabaseList[0];
                     }
 
@@ -330,22 +280,17 @@ namespace FuturaPlanConfigWarden.Windows
             }
         }
 
-
         public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "") {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private RelayCommand<object> m_restoreDatabaseCommand;
-        public RelayCommand<object> RestoreDatabaseCommand
-        {
-            get
-            {
-                return m_restoreDatabaseCommand ??= new RelayCommand<object>(_ =>
-                {
+        public RelayCommand<object> RestoreDatabaseCommand {
+            get {
+                return m_restoreDatabaseCommand ??= new RelayCommand<object>(_ => {
                     RestoreDatabase(SelectedDatabase);
-                }, _ => !string.IsNullOrEmpty(SelectedDatabase)); 
+                }, _ => !string.IsNullOrEmpty(SelectedDatabase));
             }
         }
     }
